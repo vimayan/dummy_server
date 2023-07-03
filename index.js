6d6ef76c1718cffc76a09e99ca0c9bcdf5866d47
vimayan <joblessdesks@gmail.com>
2023-07-03 09:47:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,8 @@ app.use(cors({ origin: "*" }));
 app.use(morgan("dev"));
 app.use(express.json());
 
-const lineData = {user:[30, 40, 25, 50, 49, 21, 70],guest:[15, 20, 35, 40, 39, 31, 60]};
-const pieData = {data: [44, 55, 13]};
-app.get("/", (req, res) => res.end("hello"));
-app.get("/line", (req, res) => res.json(lineData));
-app.get("/pie", (req, res) => res.json(pieData));
+const data = {lineData:{user:[30, 40, 25, 50, 49, 21, 70],guest:[15, 20, 35, 40, 39, 31, 60]},pieData: [44, 55, 13]};
+
+app.get("/", (req, res) => res.json(data));
 const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => console.log(`server listeing on port ${PORT}`));
